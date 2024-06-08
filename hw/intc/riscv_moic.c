d988630dac40ab6d8c1faf90258fbcd970042b68
zflcs <1491657576@qq.com>
2024-06-08 22:12:59 +0800

    Add register sender/receiver logic

--- hw/intc/riscv_moic.c
+++ hw/intc/riscv_moic.c
@@ -38,15 +38,171 @@ uint64_t queue_pop(Queue* queue) {
     return res;
 }
 
+void pq_init(PriorityQueue* pq) {
+    pq->task_queues = g_new0(Queue, MAX_PRIORITY);
+    int i = 0;
+    for(i = 0; i < MAX_PRIORITY; i++) {
+        QSIMPLEQ_INIT(&pq->task_queues[i].head);
+    }
+}
+
+void pq_push(PriorityQueue* pq, uint64_t priority, uint64_t data) {
+    struct QueueEntry *task_entry = g_new0(struct QueueEntry, 1);
+    task_entry->data = data;
+    QSIMPLEQ_INSERT_TAIL(&pq->task_queues[priority].head, task_entry, next);
+}
+
+uint64_t pq_pop(PriorityQueue* pq) {
+    uint64_t res = 0;
+    int i = 0;
+    for(i = 0; i < MAX_PRIORITY; i++) {
+        QueueHead *head = &pq->task_queues[i].head;
+        if (head->sqh_first != NULL) {
+            struct QueueEntry *task_entry = head->sqh_first;
+            res = task_entry->data;
+            g_free(task_entry);
+            QSIMPLEQ_REMOVE_HEAD(head, next);
+            return res;
+        }
+    }
+    return res;
+}
+
+void cap_queue_init(CapQueue* cap_queue) {
+    QSIMPLEQ_INIT(&cap_queue->head);
+}
+
+// If the cap_queue has the same item, it will do nothing.
+// Otherwise, it will insert a new capability.
+void cap_queue_insert(CapQueue* cap_queue, uint64_t task_id, 
+    uint64_t target_os_id, 
+    uint64_t target_proc_id, 
+    uint64_t target_task_id) {
+    CapQueueHead* head = &cap_queue->head;
+    struct CapQueueEntry* cur, *next_elem;
+
+    QSIMPLEQ_FOREACH_SAFE(cur, head, next, next_elem) {
+        if ((cur->cap.task_id == task_id) && (cur->cap.target.os_id == target_os_id) && (cur->cap.target.proc_id == target_proc_id)
+            && (cur->cap.target.task_id == target_task_id)) {
+            return;
+        }
+    }
+    struct CapQueueEntry *cap = g_new0(struct CapQueueEntry, 1);
+    cap->cap.task_id = task_id;
+    cap->cap.target.os_id = target_os_id;
+    cap->cap.target.proc_id = target_proc_id;
+    cap->cap.target.task_id = target_task_id;
+    QSIMPLEQ_INSERT_TAIL(head, cap, next);
+}
+
+// If the target capability is in the cap_queue, remove it.
+struct CapQueueEntry* cap_queue_remove(CapQueue* cap_queue, 
+    uint64_t task_id, 
+    uint64_t target_os_id, 
+    uint64_t target_proc_id, 
+    uint64_t target_task_id) {
+    CapQueueHead* head = &cap_queue->head;
+    struct CapQueueEntry* cur, *next_elem;
+    QSIMPLEQ_FOREACH_SAFE(cur, head, next, next_elem) {
+        if ((cur->cap.task_id == task_id) && (cur->cap.target.os_id == target_os_id) && (cur->cap.target.proc_id == target_proc_id)
+            && (cur->cap.target.task_id == target_task_id)) {
+            QSIMPLEQ_REMOVE(head, cur, CapQueueEntry, next);
+            return cur;
+        }
+    }
+    return NULL;
+}
+
+bool is_device_cap(Capability* cap) {
+    return (cap->target.os_id == 0) && (cap->target.proc_id == 0);
+}
+
 /**********************************************************/
 
 static uint64_t riscv_moic_read(void *opaque, hwaddr addr, unsigned size) {
+    RISCVMOICState *moic = opaque;
+    int idx = addr / SIZEOF_PERHART;
+    uint64_t op = addr % SIZEOF_PERHART;
+    if (op == FETCH_OP) {
+        uint64_t task_id = pq_pop(&moic->moicharts[idx].ready_queue);
+        if (task_id != 0) {
+            moic->moicharts[idx].current.task_id = task_id;
+            return task_id;
+        } else {
+            // steal a task from other hart
 
+        }
+        
+    } else {
+        error_report("Operation is not supported");
+    }
     return 0;
 }
 
 static void riscv_moic_write(void *opaque, hwaddr addr, uint64_t value, unsigned size) {
+    RISCVMOICState *moic = opaque;
+    int idx = addr / SIZEOF_PERHART;
+    uint64_t op = addr % SIZEOF_PERHART;
+    if (op == ADD_OP) {
+        uint64_t priority = (value >> 1) % MAX_PRIORITY;
+        pq_push(&moic->moicharts[idx].ready_queue, priority, value);
+    } else if (op == SWITCH_OS_OP) {
+        // update the current os identity
+        moic->moicharts[idx].current.os_id = value;
+    } else if (op == SWITCH_PROC_OP) {
+        moic->moicharts[idx].current.proc_id = value;
+    } else if (op == REGISTER_RECV_TASK_OP) {
+        moic->moicharts[idx].register_receiver_transaction.task_id = value;
+    } else if (op == REGISTER_RECV_TARGET_OS_OP) {
+        moic->moicharts[idx].register_receiver_transaction.target.os_id = value;
+    } else if (op == REGISTER_RECV_TARGET_PROC_OP) {
+        moic->moicharts[idx].register_receiver_transaction.target.proc_id = value;
+    } else if (op == REGISTER_RECV_TARGET_TASK_OP) {
+        // register receiver
+        uint64_t task_id = moic->moicharts[idx].register_receiver_transaction.task_id;
+        
+        if (is_device_cap(&moic->moicharts[idx].register_receiver_transaction)) {
+            // Sender is device, so don't record the sender identity.
+            moic->moicharts[idx].device_cap[value].task_id = task_id;
+            info_report("register external irq ok");
+        } else {
+            // Sender is another task, record the sender identity.
+            // Remove the duplicate item
+            uint64_t target_os_id = moic->moicharts[idx].register_receiver_transaction.target.os_id;
+            uint64_t target_proc_id = moic->moicharts[idx].register_receiver_transaction.target.proc_id;
+            uint64_t target_task_id = moic->moicharts[idx].register_receiver_transaction.target.task_id;
+            cap_queue_insert(&moic->moicharts[idx].recv_cap, task_id, target_os_id, target_proc_id, target_task_id);
 
+        }
+
+    } else if (op == REGISTER_SEND_TASK_OP) {
+        moic->moicharts[idx].register_sender_transaction.task_id = value;
+    } else if (op == REGISTER_SEND_TARGET_OS_OP) {
+        moic->moicharts[idx].register_sender_transaction.target.os_id = value;
+    } else if (op == REGISTER_SEND_TARGET_PROC_OP) {
+        moic->moicharts[idx].register_sender_transaction.target.proc_id = value;
+    } else if (op == REGISTER_SEND_TARGET_TASK_OP) {
+        // register sender
+        uint64_t task_id = moic->moicharts[idx].register_sender_transaction.task_id;
+        uint64_t target_os_id = moic->moicharts[idx].register_sender_transaction.target.os_id;
+        uint64_t target_proc_id = moic->moicharts[idx].register_sender_transaction.target.proc_id;
+        uint64_t target_task_id = value;
+        if (target_os_id == 0 || target_proc_id == 0 || target_task_id == 0 || task_id == 0) {
+            return;
+        }
+        cap_queue_insert(&moic->moicharts[idx].send_cap, task_id, target_os_id, target_proc_id, target_task_id);
+
+    } else if (op == SEND_INTR_OS_OP) {
+        moic->moicharts[idx].send_intr_transaction.os_id = value;
+    } else if (op == SEND_INTR_PROC_OP) {
+        moic->moicharts[idx].send_intr_transaction.proc_id = value;
+    } else if (op == SEND_INTR_TASK_OP) {
+        // send intr
+        moic->moicharts[idx].send_intr_transaction.task_id = value;
+        
+    } else {
+        error_report("Operation is not supported");
+    }
 }
 
 static void riscv_moic_irq_request(void *opaque, int irq, int level) {
@@ -90,10 +246,10 @@ static void riscv_moic_realize(DeviceState *dev, Error **errp)
     moic->moicharts = g_new0(MoicHart, hart_count);
     int i = 0;
     for(i = 0; i < hart_count; i++) {
-        queue_init(&moic->moicharts[i].ready_queue);
-        moic->moicharts[i].irq_table = g_new0(uint64_t, MAX_IRQ);
-        moic->moicharts[i].send_cap = g_new0(Capability, MAX_IRQ);
-        moic->moicharts[i].recv_cap = g_new0(Capability, MAX_IRQ);
+        pq_init(&moic->moicharts[i].ready_queue);
+        cap_queue_init(&moic->moicharts[i].send_cap);
+        cap_queue_init(&moic->moicharts[i].recv_cap);
+        moic->moicharts[i].device_cap = g_new0(Capability, MAX_IRQ);
     }
 
 }
--- include/hw/intc/riscv_moic.h
+++ include/hw/intc/riscv_moic.h
@@ -11,7 +11,24 @@
 
 #define MAX_IRQ                 0x100
 #define RISCV_MOIC_MMIO_SIZE    0x1000000
+#define MAX_PRIORITY            32
+#define SIZEOF_PERHART          0x1000
 
+#define ADD_OP                          0x00
+#define FETCH_OP                        0x08
+#define SWITCH_PROC_OP                  0x10
+#define SWITCH_OS_OP                    0x18
+#define REGISTER_RECV_TASK_OP           0x20
+#define REGISTER_RECV_TARGET_OS_OP      0x28
+#define REGISTER_RECV_TARGET_PROC_OP    0x30
+#define REGISTER_RECV_TARGET_TASK_OP    0x38
+#define REGISTER_SEND_TASK_OP           0x40
+#define REGISTER_SEND_TARGET_OS_OP      0x48
+#define REGISTER_SEND_TARGET_PROC_OP    0x50
+#define REGISTER_SEND_TARGET_TASK_OP    0x58
+#define SEND_INTR_OS_OP                 0x60                
+#define SEND_INTR_PROC_OP               0x68                
+#define SEND_INTR_TASK_OP               0x70                
 
 #define TYPE_RISCV_MOIC "riscv.moic"
 
@@ -34,6 +51,14 @@ void queue_push(Queue* queue, uint64_t data);
 uint64_t queue_pop(Queue* queue);
 
 typedef struct {
+    Queue *task_queues;
+} PriorityQueue;
+
+void pq_init(PriorityQueue* pq);
+void pq_push(PriorityQueue* pq, uint64_t priority, uint64_t data);
+uint64_t pq_pop(PriorityQueue* pq);
+
+typedef struct {
     uint64_t os_id;
     uint64_t proc_id;
     uint64_t task_id;
@@ -44,12 +69,40 @@ typedef struct {
     TotalIdentity target;
 } Capability;
 
+typedef QSIMPLEQ_HEAD(, CapQueueEntry) CapQueueHead;
+
+struct CapQueueEntry {
+    Capability cap;
+    QSIMPLEQ_ENTRY(CapQueueEntry) next;
+};
+
+typedef struct {
+    CapQueueHead head;
+} CapQueue;
+
+void cap_queue_init(CapQueue* cap_queue);
+void cap_queue_insert(CapQueue* cap_queue, uint64_t task_id, 
+    uint64_t target_os_id, 
+    uint64_t target_proc_id, 
+    uint64_t target_task_id);
+
+struct CapQueueEntry* cap_queue_remove(CapQueue* cap_queue, 
+    uint64_t task_id, 
+    uint64_t target_os_id, 
+    uint64_t target_proc_id, 
+    uint64_t target_task_id);
+
+bool is_device_cap(Capability* cap);
+
 typedef struct {
     TotalIdentity current;
-    Queue ready_queue;
-    uint64_t* irq_table;
-    Capability* send_cap;
-    Capability* recv_cap;
+    PriorityQueue ready_queue;
+    Capability* device_cap;
+    CapQueue send_cap;
+    CapQueue recv_cap;
+    TotalIdentity send_intr_transaction;
+    Capability register_receiver_transaction;
+    Capability register_sender_transaction;
 } MoicHart;
 
 struct RISCVMOICState {
